8176cf9a155efa2110317587238c5021b0e78956
mattia <matia@gmail.com>
2022-03-12 15:22:38 +0100

    ciao

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path, include
-from .views import prova
+from .views import prova, getRoutes, MyTokenObtainPairView
+
+from rest_framework_simplejwt.views import ( # for login
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
-        path('prova/', prova)
+        path('prova/', prova),
+        path('', getRoutes),
+
+        path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+        path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+ 
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer): # for customizing login token claims
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        token['password'] = user.password
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes =[
+        'api/token',
+        'api/token/refresh', 
+    ]
+
+    return Response(routes)
+
 def prova(request):
     return JsonResponse({'nome': 'mattia'})
